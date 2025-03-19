115d73524dcc607174e220ed1d60bf634d6a5996
YasithRuwantha <Yasithruwantha119@gmail.com>
2025-03-19 12:46:45 +0530

    .

--- System/src/codes/DBconnect.java
+++ System/src/codes/DBconnect.java
@@ -4,10 +4,25 @@
  */
 package codes;
 
+import com.sun.jdi.connect.spi.Connection;
+
 /**
  *
  * @author User
  */
 public class DBconnect {
-    
+    public static Connection connect (){
+        
+        Connection conn = null;
+        
+        try{
+            Class.forName("com.mysql.jdbc.driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tutionclass?zeroDateTimeBehavior=CONVERT_TO_NULL [root on Default schema]","","");
+        }catch (Exception e){
+            
+        }
+        
+        
+        return conn;
+    }
 }
